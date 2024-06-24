f3319f825cabe1ab3783947ce2671e6fbdf10f79
Aadarsh Thakur <armaanthakur2308@gmail.com>
2024-06-24 18:22:34 +0530

    changed font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./GlobalRedux/provider";
 
 const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({ 
+    subsets: ["latin"],
+    weight: ['400', '600']
+   });
+
 
 export const metadata: Metadata = {
   title: "X",
@@ -17,7 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-          <body className="w-full flex justify-center bg-black text-white">
+          <body className={`w-full flex justify-center bg-black text-white ${poppins.className}`}>
             <Providers>
               {children}
             </Providers>
